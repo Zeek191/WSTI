725f77da1d8baadc0fcb79a3884363605c2ab8b4
Zeek191 <patryk@bejamas.io>
2022-01-11 20:28:48 +0100

    feat: styles setup

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import * as styles from "./styles.module.scss"
+
+const Index = () => {
+  return (
+    <div>
+      <p className={styles.p}>Test</p>
+    </div>
+  )
+}
+
+export default Index
